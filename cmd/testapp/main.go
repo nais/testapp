3f02eba628cec08cd9fc5f41710e123871ae9357
thokra-nav <85170275+thokra-nav@users.noreply.github.com>
2021-06-03 15:49:44 +0200

    Use GCP_TEAM_PROJECT_ID for bigquery

--- cmd/testapp/main.go
+++ cmd/testapp/main.go
@@ -54,7 +54,7 @@ func init() {
 	flag.StringVar(&bindAddr, "bind-address", ":8080", "ip:port where http requests are served")
 	flag.StringVar(&pingResponse, "ping-response", "pong\n", "what to respond when pinged")
 	flag.StringVar(&bucketName, "bucket-name", os.Getenv("BUCKET_NAME"), "name of bucket used with /{read,write}bucket")
-	flag.StringVar(&projectID, "projectid", os.Getenv("PROJECT_ID"), "projectid used with /{read,write}bigquery")
+	flag.StringVar(&projectID, "projectid", os.Getenv("GCP_TEAM_PROJECT_ID"), "projectid used with /{read,write}bigquery")
 	flag.StringVar(&bigqueryName, "bigquery-name", os.Getenv("BIGQUERY_NAME"), "name of bigquery dataset used with /{read,write}bigquery")
 	flag.StringVar(&bucketObjectName, "bucket-object-name", "test", "name of bucket object used with /{read,write}bucket")
 	flag.StringVar(&connectURL, "connect-url", "https://google.com", "URL to connect to with /connect")
