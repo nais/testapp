e9450a1646c9756447eb5d1c3e3548f1608534ff
Roger Bjørnstad <roger.bjornstad@nav.no>
2024-07-10 10:39:11 +0200

    chore: put back deploy metrics because deploy-canary

--- cmd/testapp/main.go
+++ cmd/testapp/main.go
@@ -6,17 +6,20 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
+	"github.com/nais/testapp/internal/metrics"
 	"github.com/prometheus/common/version"
 	log "github.com/sirupsen/logrus"
 	flag "github.com/spf13/pflag"
 )
 
 var (
-	bindAddr     string
-	pingResponse string
-	connectURL   string
+	bindAddr             string
+	pingResponse         string
+	connectURL           string
+	deployStartTimestamp int64
 )
 
 func init() {
@@ -25,10 +28,25 @@ func init() {
 	flag.StringVar(&bindAddr, "bind-address", ":8080", "ip:port where http requests are served")
 	flag.StringVar(&pingResponse, "ping-response", "pong\n", "what to respond when pinged")
 	flag.StringVar(&connectURL, "connect-url", "https://google.com", "URL to connect to with /connect")
+	flag.Int64Var(&deployStartTimestamp, "deploy-start-time", getEnvInt("DEPLOY_START", time.Now().UnixNano()), "unix timestamp with nanoseconds, specifies when NAIS deploy of testapp started")
 	flag.Parse()
 }
 
 func main() {
+	metrics.StartTimestamp.SetToCurrentTime()
+	metrics.DeployTimestamp.Set(float64(deployStartTimestamp) / 10e8)
+
+	metrics.LeadTime.Set(timeSinceDeploy())
+	metrics.TimeSinceDeploy.Set(timeSinceDeploy())
+	tick := time.NewTicker(time.Second)
+	go func() {
+		for range tick.C {
+			metrics.TimeSinceDeploy.Set(timeSinceDeploy())
+		}
+	}()
+
+	http.Handle("/metrics", metrics.Handler())
+
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		param := r.URL.Query().Get("delay")
 		if d, err := time.ParseDuration(param); err == nil {
@@ -91,4 +109,18 @@ func main() {
 	if err := http.ListenAndServe(bindAddr, nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getEnvInt(key string, fallback int64) int64 {
+	if value, ok := os.LookupEnv(key); ok {
+		i, _ := strconv.ParseInt(value, 10, 64)
+		return i
+	}
+
+	return fallback
+}
+
+func timeSinceDeploy() float64 {
+	deployStartTime := time.Unix(0, deployStartTimestamp)
+	return time.Since(deployStartTime).Seconds()
 }
---
+++ internal/metrics/metrics.go
@@ -0,0 +1,51 @@
+package metrics
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+const (
+	namespace = "nais"
+	subsystem = "testapp"
+)
+
+func gauge(name, help string) prometheus.Gauge {
+	return prometheus.NewGauge(prometheus.GaugeOpts{
+		Name:      name,
+		Help:      help,
+		Namespace: namespace,
+		Subsystem: subsystem,
+	})
+}
+
+func hist(name, help string, buckets []float64) prometheus.Histogram {
+	return prometheus.NewHistogram(prometheus.HistogramOpts{
+		Namespace: namespace,
+		Subsystem: subsystem,
+		Name:      name,
+		Help:      help,
+		Buckets:   buckets,
+	})
+}
+
+var (
+	LeadTime        = gauge("lead_time", "Seconds used in deployment pipeline, from making the request until the application is available")
+	TimeSinceDeploy = gauge("time_since_deploy", "Seconds since the latest deploy of this application")
+
+	DeployTimestamp = gauge("deploy_timestamp", "Timestamp when the deploy of this application was triggered in the pipeline")
+	StartTimestamp  = gauge("start_timestamp", "Start time of the application")
+)
+
+func init() {
+	prometheus.MustRegister(LeadTime)
+	prometheus.MustRegister(TimeSinceDeploy)
+	prometheus.MustRegister(DeployTimestamp)
+	prometheus.MustRegister(StartTimestamp)
+}
+
+func Handler() http.Handler {
+	return promhttp.Handler()
+}
